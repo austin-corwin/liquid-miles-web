953509dc9b36e13c1224c27afdfe1d44665f8c5c
Austin Corwin <austincorwin5@gmail.com>
2025-01-02 16:36:17 -0600

    Initialize new liquid miles project

---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,5 @@
+const Navigation = () => {
+  return <div className='w-full h-16 bg-primary'></div>;
+};
+
+export default Navigation;
---
+++ src/features/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = ({}) => {
+  return (
+    <div className='w-full flex justify-center items-center h-screen'>
+      <h1>Welcome to Liquid Miles</h1>
+    </div>
+  );
+};
+
+export default Hero;
