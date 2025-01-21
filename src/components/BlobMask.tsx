1ca3785c40b47c4480dbc68ea42fbae252678ca1
Austin Corwin <austincorwin5@gmail.com>
2025-01-21 16:05:48 -0500

    Add blob mask component

---
+++ src/components/BlobMask.tsx
@@ -0,0 +1,30 @@
+const BlobMask = ({ path, id }: { path: string; id: string }) => {
+  return (
+    <svg
+      id='10015.io'
+      viewBox='0 0 480 480'
+      xmlns='http://www.w3.org/2000/svg'
+      xmlnsXlink='http://www.w3.org/1999/xlink'
+    >
+      <defs>
+        <clipPath id={id}>
+          <path
+            fill='#474bff'
+            d='M460,280Q447,320,427.5,356.5Q408,393,375,418.5Q342,444,302,459Q262,474,218.5,472Q175,470,139.5,445Q104,420,79,387.5Q54,355,32.5,319Q11,283,10,240Q9,197,31.5,161Q54,125,78.5,91.5Q103,58,139,35.5Q175,13,218,15.5Q261,18,301,27.5Q341,37,374,62.5Q407,88,430.5,122.5Q454,157,463.5,198.5Q473,240,460,280Z'
+          />
+        </clipPath>
+      </defs>
+      <image
+        x='0'
+        y='0'
+        width='100%'
+        height='100%'
+        clipPath={`url(#${id})`}
+        xlinkHref={path}
+        preserveAspectRatio='xMidYMid slice'
+      ></image>
+    </svg>
+  );
+};
+
+export default BlobMask;
--- src/features/Location.tsx
+++ src/features/Location.tsx
@@ -1,32 +1,14 @@
+import BlobMask from "@/components/BlobMask";
+
 const Location = () => {
   return (
     <div className='bg-white w-full flex justify-center py-12 pb-24'>
       <div className='container w-full flex gap-12'>
         <div className='w-full max-w-[500px]'>
-          <svg
-            id='10015.io'
-            viewBox='0 0 480 480'
-            xmlns='http://www.w3.org/2000/svg'
-            xmlnsXlink='http://www.w3.org/1999/xlink'
-          >
-            <defs>
-              <clipPath id='blob'>
-                <path
-                  fill='#474bff'
-                  d='M460,280Q447,320,427.5,356.5Q408,393,375,418.5Q342,444,302,459Q262,474,218.5,472Q175,470,139.5,445Q104,420,79,387.5Q54,355,32.5,319Q11,283,10,240Q9,197,31.5,161Q54,125,78.5,91.5Q103,58,139,35.5Q175,13,218,15.5Q261,18,301,27.5Q341,37,374,62.5Q407,88,430.5,122.5Q454,157,463.5,198.5Q473,240,460,280Z'
-                />
-              </clipPath>
-            </defs>
-            <image
-              x='0'
-              y='0'
-              width='100%'
-              height='100%'
-              clipPath='url(#blob)'
-              xlinkHref={"../../images/neighborhoodRoute.jpg"}
-              preserveAspectRatio='xMidYMid slice'
-            ></image>
-          </svg>
+          <BlobMask
+            path='../../images/neighborhoodRoute.jpg'
+            id='neighborhoodRoute'
+          />
         </div>
         <section className='gap-4 flex flex-col text-secondary max-w-[700px] justify-center'>
           <div>
