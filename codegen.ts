6cd02bb71a3bdf0d4ef7400cc3e47d579031b017
Eric Nowels <ericnowels@gmail.com>
2025-03-09 16:57:06 -0600

    add codegen scripts

---
+++ codegen.ts
@@ -0,0 +1,29 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+import { loadEnvConfig } from '@next/env'
+
+const projectDir = process.cwd()
+loadEnvConfig(projectDir)
+
+const config: CodegenConfig = {
+	overwrite: true,
+	schema: {
+		// [`${CMS_API}${process.env.CONTENTFUL_SPACE_ID}`]: {
+		[`https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`]:
+			{
+				headers: {
+					'User-Agent': 'Codegen',
+					Authorization: `Bearer ${process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN}`,
+				},
+			},
+	},
+	generates: {
+		'src/api/gql/': {
+			preset: 'client',
+		},
+		'src/api/gql/schema.gql': {
+			plugins: ['schema-ast'],
+		},
+	},
+}
+
+export default config
--- src/utils/contentful/constants.ts
+++ src/utils/contentful/constants.ts
@@ -1,3 +1,4 @@
-export const EXAMPLE_PATH = "cms-contentful";
-export const CMS_NAME = "Contentful";
-export const CMS_URL = "https://www.contentful.com";
+export const EXAMPLE_PATH = 'cms-contentful'
+export const CMS_NAME = 'Contentful'
+export const CMS_URL = 'https://www.contentful.com'
+export const CMS_API = `https://graphql.contentful.com/content/v1/spaces/`
