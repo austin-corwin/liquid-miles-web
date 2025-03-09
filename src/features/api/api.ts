36804718a1cef3d456dcab02edeeb72c264b1eba
Eric Nowels <ericnowels@gmail.com>
2025-03-09 16:57:25 -0600

    stub api helper

---
+++ src/features/api/api.ts
@@ -0,0 +1,48 @@
+'use server'
+
+interface ApiResponse<D = unknown> {
+	data: D
+	success: boolean
+	errors?: unknown | null
+}
+
+export const fetchGraphQL = async <D = unknown>(
+	query: string,
+	variables?: { [key: string]: unknown },
+	preview = false
+): Promise<ApiResponse<D>> => {
+	const response: ApiResponse<D> = {
+		data: null as D,
+		errors: null,
+		success: false,
+	}
+	try {
+		const request = await fetch(
+			`https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${
+						preview
+							? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
+							: process.env.CONTENTFUL_ACCESS_TOKEN
+					}`,
+				},
+				body: JSON.stringify({ query, variables }),
+				next: { tags: ['posts'] },
+			}
+		)
+		const { data } = await request.json()
+		if (data) {
+			console.log(data)
+			response.data = data
+			response.success = true
+		}
+	} catch (error: unknown) {
+		console.warn('GraphQL API Request failed', error)
+		response.errors = error
+	} finally {
+		return response
+	}
+}
