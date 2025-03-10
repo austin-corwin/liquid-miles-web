071e163981d3ad139ce99918e45614bdad1c7272
Eric Nowels <ericnowels@gmail.com>
2025-03-09 18:46:28 -0600

    make page content legible

--- src/app/(nonAuth)/[[...slug]]/page.tsx
+++ src/app/(nonAuth)/[[...slug]]/page.tsx
@@ -1,4 +1,5 @@
 import { PageCollection } from '@/api/gql/graphql'
+import Wave from '@/components/Wave'
 import { fetchGraphQL } from '@/features/api/api'
 import { getPageBySlugQuery } from '@/features/api/queries'
 
@@ -21,13 +22,13 @@ export const revalidate = false // only rebuild cache on manual revalidations
 // generate metadata for SEO
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
 	const requestParams = await params
+	// @TODO: abstract this function for use by any content type
 	const slug = requestParams?.slug?.join('/')
 	const { data } = await fetchGraphQL<PageCollection>(getPageBySlugQuery, {
 		slug,
 	})
 	const page = data?.items?.shift()
 	if (!page && slug) notFound()
-
 	const plainContent = documentToPlainTextString(page?.subtitle?.json) // -> Hello world!
 
 	const metaData: Metadata = {
@@ -56,30 +57,37 @@ export default async function Page({ params }: Props) {
 	const faqs = page?.faqsCollection?.items
 
 	return (
-		<div className='flex flex-col w-full gap-8'>
-			<header className='px-6 py-10'>
-				<h1 className='w-full mb-12 text-2xl lg:text-[6rem] font-extrabold font-primary uppercase text-white border-b-4 border-white lg:leading-[5rem] pb-3 '>
-					{page.title}
-				</h1>
-				{page?.subtitle && (
-					<h2 className='text-2xl'>
-						<Markdown content={page.subtitle as Content} />
-					</h2>
-				)}
+		<div className='flex flex-col w-full gap-16'>
+			<header className='px-6 py-16 bg-secondary'>
+				<div className='flex flex-col gap-8 mx-auto container'>
+					<h1 className='w-full mb-12 text-2xl lg:text-[6rem] font-extrabold font-primary uppercase text-white border-b-4 border-white lg:leading-[5rem] pb-3 container'>
+						{page.title}
+					</h1>
+					{page?.subtitle && (
+						<h2 className='text-2xl'>
+							<Markdown content={page.subtitle as Content} />
+						</h2>
+					)}
+				</div>
 			</header>
-			<div>
+			<div className='container mx-auto text-lg'>
 				<Markdown content={page.content as Content} />
 			</div>
 			{faqs?.length > 0 && (
-				<section className='bg-gray-200 p-6'>
-					<h2>FAQs</h2>
-					<ol className='w-full text-xl text-white flex flex-col justify-center pl-12 pr-10 gap-12 h-full'>
-						{faqs.map((faq) => (
-							<FaqItem key={faq?._id} item={faq} />
-						))}
-					</ol>
+				<section className='p-6'>
+					<div className='flex flex-col container mx-auto'>
+						<h2 className='text-3xl text-secondary font-bold pb-4'>
+							FAQs
+						</h2>
+						<ol className='w-full text-xl text-secondary flex flex-col justify-center pl-12 pr-10 gap-12 h-full'>
+							{faqs.map((faq) => (
+								<FaqItem key={faq?._id} item={faq} />
+							))}
+						</ol>
+					</div>
 				</section>
 			)}
+			<Wave fill='#ffc800' />
 		</div>
 	)
 }
