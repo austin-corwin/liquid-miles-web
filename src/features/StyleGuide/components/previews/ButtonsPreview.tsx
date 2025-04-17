44209447b3795cd51fd92d741d3774135e62dd3d
Eric Nowels <ericnowels@gmail.com>
2025-04-17 16:44:54 -0600

    add variants and color schemes to button previews

--- src/features/StyleGuide/components/previews/ButtonsPreview.tsx
+++ src/features/StyleGuide/components/previews/ButtonsPreview.tsx
@@ -1,39 +1,67 @@
 'use client'
 
-import { Button, ButtonGroup, ButtonProps, VStack } from '@chakra-ui/react'
+import { headingRecipes } from '@/features/chakra-ui/config/recipes'
+import {
+  Button,
+  ButtonGroup,
+  ButtonProps,
+  Heading,
+  VStack,
+} from '@chakra-ui/react'
 import React from 'react'
 import { UiComponentPreview } from '../../config/uiSectionsConfig'
 
 const ButtonsPreview: React.FC = () => {
-  const buttonVariants: ButtonProps['variant'] = [
+  const buttonVariants: ButtonProps['variant'][] = [
     'solid',
     'outline',
     'ghost',
     'link',
     'unstyled',
   ]
+  const colorScheme: ButtonProps['colorScheme'][] = ['gray', 'yellow', 'teal']
 
   return (
     <>
-      <VStack>
-        {buttonVariants.map((variant) => (
-          <ButtonGroup key={variant}>
-            <Button variant={variant}> Solid Button </Button>
-            <Button variant={variant} disabled>
-              Disabled
-            </Button>
-          </ButtonGroup>
-        ))}
+      <VStack alignItems='start' gap={12}>
+        <VStack alignItems='start' gap={4}>
+          <Heading {...headingRecipes.H6} pb={4}>
+            Variants
+          </Heading>
+          {buttonVariants.map((variant) => (
+            <ButtonGroup key={String(variant)}>
+              <Button variant={variant} textTransform='uppercase'>
+                {String(variant)} Button
+              </Button>
+              <Button variant={variant} isActive>
+                Active
+              </Button>
+              <Button variant={variant}>Active</Button>
+              <Button variant={variant} disabled>
+                Disabled
+              </Button>
+            </ButtonGroup>
+          ))}
+        </VStack>
+        <VStack alignItems='start' gap={4}>
+          <Heading {...headingRecipes.H6} pb={4}>
+            Color Schemes
+          </Heading>
+          {colorScheme.map((colorScheme) => (
+            <ButtonGroup key={String(colorScheme)}>
+              <Button colorScheme={colorScheme} textTransform='uppercase'>
+                {colorScheme} Button
+              </Button>
+              <Button colorScheme={colorScheme} isActive>
+                Active
+              </Button>
+              <Button colorScheme={colorScheme} disabled>
+                Disabled
+              </Button>
+            </ButtonGroup>
+          ))}
+        </VStack>
       </VStack>
-      <ButtonGroup>
-        <Button variant='solid'> Solid Button </Button>
-        <Button variant='solid' disabled>
-          Disabled
-        </Button>
-      </ButtonGroup>
-      <Button variant='outline'> Outline Button </Button>
-      <Button variant='ghost'> Ghost Button </Button>
-      <Button variant='link'> Link Button </Button>
     </>
   )
 }
