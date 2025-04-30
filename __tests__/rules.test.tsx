d34db00da96583c663797f7c80189e3bc323a687
Eric Nowels <ericnowels@gmail.com>
2025-04-29 21:46:52 -0600

    colocate test files

--- __tests__/rules.test.tsx
+++
@@ -1,8 +0,0 @@
-import RulesPage from '@/app/(nonAuth)/(informational)/rules/page'
-import { render, screen } from '@testing-library/react'
-import { expect, test } from 'vitest'
-
-test('Home Page', () => {
-  render(<RulesPage />)
-  expect(screen.getByRole('heading', { level: 1, name: 'Rules' })).toBeDefined()
-})
